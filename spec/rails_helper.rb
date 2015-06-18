c4b7e6909f95cf178d6dfafe77031613d04db97e
Jordan Kamin <jordanakamin@gmail.com>
2015-06-18 09:53:43 -0500

    fixes broken feature tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,9 @@ require_relative 'support/wait_for_ajax.rb'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
 
 DatabaseCleaner.strategy = :truncation
 
