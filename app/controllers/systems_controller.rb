72c10e9ceaba8dd08238781f31ade33236cdebc5
Jordan Kamin <jordanakamin@gmail.com>
2015-06-21 16:11:40 -0700

    WIP small styling updates

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -18,6 +18,8 @@ class SystemsController < ApplicationController
 
   def show
     @system = System.find(params[:id])
+    p params
+    @user = User.find(@system.user_id)
     render partial: 'show'
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 require 'faker'
-System.create(name: Faker::Lorem.word, description: Faker::Lorem.paragraph)
 user = User.new(name: "user", password: ENV["PASSWORD"])
 user.save!
+System.create(name: Faker::Lorem.word, description: Faker::Lorem.paragraph, user_id: User.last.id)
