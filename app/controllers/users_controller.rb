cdbb82f658865f66b651ebb82e9f8b16309fc265
Jordan Kamin <apprentice@Jordan-Ghost-Werewolf-Superman.local>
2015-06-27 13:07:48 -0700

    adds bio to strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:password)
+    params.require(:user).permit(:password, :bio)
   end
 
 end
