37c522fff27b3ee942363f308843bc0d1291f85d
Jordan Kamin <apprentice@Jordan-Ghost-Werewolf-Superman.local>
2015-06-28 11:15:06 -0700

    defaults user pic to no-image.png

--- db/migrate/20150625191652_add_profile_info_to_users.rb
+++ db/migrate/20150625191652_add_profile_info_to_users.rb
@@ -1,6 +1,6 @@
 class AddProfileInfoToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :picture, :string
+    add_column :users, :picture, :string, default: "no-image.png"
     add_column :users, :bio, :text
     add_column :users, :birthday, :date
   end
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20150625191652) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "picture"
+    t.string   "picture",         default: "no-image.png"
     t.text     "bio"
     t.date     "birthday"
   end
