fa96d33e468351a1ed6d666a5467527e400d5ddd
Jordan Kamin <apprentice@Jordan-Ghost-Werewolf-Superman.local>
2015-06-29 13:02:48 -0700

    adds tests for new validations on user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,9 @@ describe User do
     it { should validate_uniqueness_of :name }
     it { should have_many :systems }
     it { should have_secure_password }
+    it { should have_attached_file(:avatar)}
+    it { should validate_attachment_content_type(:avatar).
+                    allowing('image/png', 'image/gif', 'image/jpg').
+                    rejecting('text/plain', 'text/xml') }
   end
 end
