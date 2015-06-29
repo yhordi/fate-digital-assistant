99b6af9c5caaf26f3f947a21cb30f619b4c2f91c
Jordan Kamin <apprentice@Jordan-Ghost-Werewolf-Superman.local>
2015-06-29 11:52:23 -0700

    updates/uploads profile pic correctly

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,11 +8,13 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_host_name => 's3-us-west-1.amazonaws.com',
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] ,
+      :url => 'http://fate-dimensions.s3.amazonaws.com'
+    },
+    :bucket => ENV['S3_BUCKET_NAME'],
   }
   
   # Do not eager load code on boot.
