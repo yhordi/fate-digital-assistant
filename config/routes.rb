2ff13ca4c1cc7fb3c4b8191adda25d3230f36148
Jordan Kamin <apprentice@Jordan-Ghost-Werewolf-Superman.local>
2015-07-12 12:44:29 -0700

    adds location links to systems show

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users, except: :index
   resources :sessions, only: [:create, :destroy, :show]
   resources :systems
+  resources :locations, except: [:index, ]
   root 'users#new'
 end
