a90d02353cebd19716895d37217e3bbd6f1c2ed8
Jordan Kamin <jordanakamin@gmail.com>
2016-03-04 12:09:25 -0800

    Moves migration code from model to migration

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,3 @@
 class Skill < ActiveRecord::Base
-  t.string :name, null: false
-  t.integer :level, null: false, default: 0
-  t.text :description
+
 end
--- db/migrate/20160304194835_create_skills.rb
+++ db/migrate/20160304194835_create_skills.rb
@@ -1,7 +1,9 @@
 class CreateSkills < ActiveRecord::Migration
   def change
     create_table :skills do |t|
-
+      t.string :name, null: false
+      t.integer :level, null: false, default: 0
+      t.text :description
       t.timestamps null: false
     end
   end
