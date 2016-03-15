644dfed335868a9d99b9fdead3acc6a5c7437996
Jordan Kamin <jordanakamin@gmail.com>
2016-03-15 13:40:49 -0700

    removes .card css class and divs

---
+++ app/assets/javascripts/components/system_link.js.jsx
@@ -0,0 +1,13 @@
+var SystemLink = React.createClass({
+  handleLink: function(e) {
+    console.log(e)
+    e.preventDefault(e)
+  },
+  render: function() {
+      return (
+        <div key={system.id}>
+          <a onClick={this.handleLink}  className='systemLink' href='#'>{system.name}</a>
+        </div>
+      )
+  }
+});
