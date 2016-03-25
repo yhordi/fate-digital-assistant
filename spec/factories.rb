6e9019ea592367137b8943b838eea7c2cd47bbba
Jordan Kamin <jordanakamin@gmail.com>
2016-03-25 12:08:48 -0700

    Adds tests for SkillsController#index

--- spec/factories.rb
+++ spec/factories.rb
@@ -12,5 +12,9 @@ FactoryGirl.define do
     name { Faker::Lorem.word }
     description { Faker::Lorem.paragraph }
     system_id { 1 }
+    overcome {Faker::Lorem.paragraph}
+    advantage {Faker::Lorem.paragraph}
+    attack {Faker::Lorem.paragraph}
+    defend {Faker::Lorem.paragraph}
   end
 end
