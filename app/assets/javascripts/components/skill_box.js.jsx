3dd254e1543c4746fa42e9efe1410e9e6b732197
Jordan Kamin <jordanakamin@gmail.com>
2016-03-28 12:49:57 -0700

    Adds breadcrumbs to skill_box

--- app/assets/javascripts/components/skill_box.js.jsx
+++ app/assets/javascripts/components/skill_box.js.jsx
@@ -2,17 +2,26 @@ var SkillBox = React.createClass({
   render: function() {
     return (
       <div>
-        <ul>
-          <li>
-            <a href="#">
-            </a>
-          </li>
-          <li>
-            <a href="#">
-            </a>
-          </li>
-        </ul>
-        <h2>{this.props.systemName}</h2>
+        <h2 className="no-margin">
+          {this.props.systemName}
+        </h2>
+        <div className="m-btm-m">
+          <ul className="list-hz">
+            <li>
+              <a href="#">
+                My Systems
+              </a>/
+            </li>
+            <li>
+              <a href="#">
+                {this.props.systemName}
+              </a>/
+            </li>
+            <li>
+              Skills List
+            </li>
+          </ul>
+        </div>
         <SkillList data={this.props.data} />
       </div>
     );
