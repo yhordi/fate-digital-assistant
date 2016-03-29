b4c43f71f8dd2b298ba60922db1e5ad35b05e417
Jordan Kamin <jordanakamin@gmail.com>
2016-03-29 10:59:45 -0700

    Adds listeners to fields in SkillForm

--- app/assets/javascripts/components/skill_form.js.jsx
+++ app/assets/javascripts/components/skill_form.js.jsx
@@ -1,30 +1,40 @@
 var SkillForm = React.createClass({
+  getInitialState: function() {
+    return {}
+  },
+  updateState: function(e) {
+    var prop = e.target.name
+    var value = e.target.value
+    var skill = {}
+    skill[prop] = value
+    this.setState(skill)
+  },
   render: function() {
     return(
       <form>
           <label for="skillName">Skill Name</label>
         <div>
-          <input id="skillName" name="name" />
+          <input onChange={this.updateState} id="skillName" name="name" />
         </div>
           <label for="skillDescription">Description</label>
         <div>
-          <textarea id="skillDescription" name="description"></textarea>
+          <textarea onChange={this.updateState} id="skillDescription" name="description"></textarea>
         </div>
           <label for="advantage">Create an Advantage</label>
         <div>
-          <textarea id="advantage" name="advantage"></textarea>
+          <textarea onChange={this.updateState} id="advantage" name="advantage"></textarea>
         </div>
           <label for="overcome">Overcome</label>
         <div>
-          <textarea id="overcome" name="overcome"></textarea>
+          <textarea onChange={this.updateState} id="overcome" name="overcome"></textarea>
         </div>
           <label for="attack">attack</label>
         <div>
-          <textarea id="attack" name="attack"></textarea>
+          <textarea onChange={this.updateState} id="attack" name="attack"></textarea>
         </div>
           <label for="defend">defend</label>
         <div>
-          <textarea id="defend" name="defend"></textarea>
+          <textarea onChange={this.updateState} id="defend" name="defend"></textarea>
         </div>
         <input type="submit" />
       </form>
