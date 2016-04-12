91f16ba6a16d5ce5c943ec6800471779539fd661
Jordan Kamin <jordanakamin@gmail.com>
2016-04-12 13:10:32 -0700

    builds functionality for systems link in sidebar

--- app/assets/javascripts/components/side_bar.js.jsx
+++ app/assets/javascripts/components/side_bar.js.jsx
@@ -1,14 +1,20 @@
 var SideBar = React.createClass({
-
+  container: function(){
+   return document.getElementById('container')
+  },
+  getSystems: function(e){
+    e.preventDefault
+    ReactDOM.unmountComponentAtNode(this.container())
+  },
   render: function() {
     return (
     <div className="nav">
       <ul className="navlist">
         <li className="navItem">
-          <a>SYSTEMS</a>
+          <a onClick={this.getSystems} href='/systems/'>SYSTEMS</a>
         </li>
         <li className="navItem">
-          <a>GAMES</a>
+          <p>GAMES</p>
         </li>
         <li className="navItem">
           <a href={"/users/" + this.props.userId}>PROFILE</a>
