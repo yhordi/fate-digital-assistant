85a47834463248e8a2baab5fff5d49b851cdd48c
Jordan Kamin <jordanakamin@gmail.com>
2016-04-18 15:16:53 -0700

    builds link to system in skill list

--- app/assets/javascripts/components/skill_box.js.jsx
+++ app/assets/javascripts/components/skill_box.js.jsx
@@ -3,7 +3,7 @@ var SkillBox = React.createClass({
     e.preventDefault(e)
     var container = document.getElementById('container')
     ReactDOM.unmountComponentAtNode(container)
-    var url = 'systems/' + this.props.systemId
+    var url = '/systems/' + this.props.systemId
     $.ajax({
       url: url,
       success: function(data){
@@ -25,7 +25,7 @@ var SkillBox = React.createClass({
     return (
       <div className='row'>
         <h2 className="no-margin">
-          <a>{this.props.systemName}</a>
+          <a onClick={this.handleBackToSystem}>{this.props.systemName}</a>
         </h2>
         <SkillList systemId={this.props.systemId} systemName={this.props.systemName} data={this.props.data} />
       </div>
