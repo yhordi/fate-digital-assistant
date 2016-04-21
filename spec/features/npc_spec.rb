89043406387de8a9a199304737fa4a7e6155490e
Jordan Kamin <jordanakamin@gmail.com>
2016-04-21 15:19:05 -0700

    adds feature test for deleting and npc

--- spec/features/npc_spec.rb
+++ spec/features/npc_spec.rb
@@ -15,9 +15,17 @@ describe 'Npc', js: true do
     it 'can see npcs on the npc index' do
       expect(page).to have_content(npc.name)
     end
-    it 'can click on an npc link and see an npc profile' do
-      page.find('#npc1').click
-      expect(page).to have_content(npc.background)
+    context 'on the npc show page' do
+      before(:each) do
+        page.find('#npc1').click
+      end
+      it 'can see an npc profile' do
+        expect(page).to have_content(npc.background)
+      end
+      it 'can delete an npc and see the list of remaining npcs' do
+        click_button 'Delete'
+        expect(page).to_not have_content(npc.name)
+      end
     end
   end
 
