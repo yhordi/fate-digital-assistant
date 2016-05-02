6badd05964df5479469647a89dbd5e3aeb91aaf2
Jordan Kamin <jordanakamin@gmail.com>
2016-05-02 13:38:33 -0700

    builds frontend to add skill to npc

--- app/assets/javascripts/components/skills_checklist.js.jsx
+++ app/assets/javascripts/components/skills_checklist.js.jsx
@@ -1,4 +1,8 @@
 var SkillsChecklist = React.createClass({
+  addSkillToNpc: function(e){
+    e.preventDefault()
+    var url = '/systems/'
+  },
   render: function() {
     var checkboxes = this.props.data.map(function(skill, index) {
       return(
@@ -8,18 +12,20 @@ var SkillsChecklist = React.createClass({
     return(
       <div>
         <div>
-          <select>
-            {checkboxes}
-          </select>
-          <select name='level'>
-            <option value='0'>Untrained</option>
-            <option value='1'>1</option>
-            <option value='2'>2</option>
-            <option value='3'>3</option>
-            <option value='4'>4</option>
-            <option value='5'>5</option>
-            <option value='6'>6+</option>
-          </select>
+          <form onSubmit={this.addSkillToNpc}>
+            <select>
+              {checkboxes}
+            </select>
+            <select name='level'>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+              <option value='6'>6+</option>
+            </select>
+            <input type='submit' />
+          </form>
         </div>
       </div>
     )
