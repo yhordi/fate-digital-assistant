e528be8e39d0256a24e8e4035d6e9dd11c40ad55
Jordan Kamin <jordanakamin@gmail.com>
2016-05-05 13:42:38 -0700

    Adds method to return skill names from Skill

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,12 @@
 class Skill < ActiveRecord::Base
   belongs_to :system
   validates :name, presence: true
+
+  def self.names(system_id)
+    names = []
+    Skill.where(system_id: system_id).each do |skill|
+      names << skill.name
+    end
+    names
+  end
 end
