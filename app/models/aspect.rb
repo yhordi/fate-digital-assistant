8ff21411d03fb973329ea63afb347c900cab1a56
Jordan Kamin <jordanakamin@gmail.com>
2016-06-23 16:55:53 -0700

    Adds model tests for aspect, validations

--- app/models/aspect.rb
+++ app/models/aspect.rb
@@ -1,3 +1,4 @@
 class Aspect < ActiveRecord::Base
   belongs_to :aspectable, polymorphic: true
+  validates :name, :description, presence: true
 end
---
+++ spec/models/aspect_spec.rb
@@ -0,0 +1,6 @@
+describe Aspect do
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :description }
+  end
+end
