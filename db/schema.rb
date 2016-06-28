ba082683763dc982fd129cf9186b81eb5ff1c219
Jordan Kamin <jordanakamin@gmail.com>
2016-06-27 17:26:00 -0700

    Adds stress fields to Npc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623183242) do
+ActiveRecord::Schema.define(version: 20160628002335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,10 @@ ActiveRecord::Schema.define(version: 20160623183242) do
     t.string   "npc_type"
     t.text     "background"
     t.integer  "system_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "mental_stress",   default: 2
+    t.integer  "physical_stress", default: 2
   end
 
   create_table "skills", force: :cascade do |t|
