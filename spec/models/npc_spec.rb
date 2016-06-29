3faf9c4d911c642944ed4d60ebdf3305d43ce55e
Jordan Kamin <jordanakamin@gmail.com>
2016-06-29 10:53:15 -0700

    builds out other cases for unit tests of stress calculation

--- spec/models/npc_spec.rb
+++ spec/models/npc_spec.rb
@@ -1,20 +1,35 @@
 describe Npc do
   let(:npc) { FactoryGirl.create :npc }
-  let(:physique) { CharacterSkill.new(name: 'Physique', level: 5)}
-  let(:will) { CharacterSkill.new(name: 'Will', level: 5)}
+  let(:physique) { CharacterSkill.new(name: 'Physique', level: 0)}
+  let(:will) { CharacterSkill.new(name: 'Will', level: 0)}
 
   describe 'validations' do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :npc_type }
   end
   describe '#calculate_max_stress' do
-    it 'adjusts the max_mental_stress for an npc based on a physique level of 5' do
-      npc.character_skills << physique
-      expect{npc.calculate_max_stress}.to change{npc.max_physical_stress}.by(3)
-    end
-    it 'adjusts the max_physical_stress for an npc based on will level of 5' do
-      npc.character_skills << will
-      expect{npc.calculate_max_stress}.to change{npc.max_mental_stress}.by(3)
+    levels = [1,2,3,5]
+    levels.each do |level|
+      it "adjusts the max_mental_stress for an npc based on a physique level of #{level}" do
+        physique.level = level
+        if level.between?(3, 4)
+          level = 2
+        elsif level == 5
+          level = 3
+        end
+        npc.character_skills << physique
+        expect{npc.calculate_max_stress}.to change{npc.max_physical_stress}.by(level)
+      end
+      it "adjusts the max_physical_stress for an npc based on will level of #{level}" do
+        will.level = level
+        if level.between?(3, 4)
+          level = 2
+        elsif level == 5
+          level = 3
+        end
+        npc.character_skills << will
+        expect{npc.calculate_max_stress}.to change{npc.max_mental_stress}.by(level)
+      end
     end
   end
 end
