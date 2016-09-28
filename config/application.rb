b32585ca99f1afc7581b153e66a7bc01602f5d8f
Jordan Kamin <jordanakamin@gmail.com>
2016-09-28 11:24:53 -0700

    Autoloads concerns folder

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ Bundler.require(*Rails.groups)
 
 module FateDigitalAssistant
   class Application < Rails::Application
+    config.autoload_paths += %W(
+      #{config.root}/app/controllers/concerns
+      #{config.root}/app/models/concerns
+    )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
