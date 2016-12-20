fca80fe8488cb30223de7a2a577e35a42a0681a5
Jordan Kamin <jordanakamin@gmail.com>
2016-12-20 11:20:31 -0800

    tests ScenesController#update

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-
 gem 'rails', '4.2'
 gem 'pg'
 gem 'sass-rails', '~> 4.0.3'
--- spec/controllers/scenes_controller_spec.rb
+++ spec/controllers/scenes_controller_spec.rb
@@ -1,13 +1,22 @@
 describe ScenesController do
   let(:scenario) { FactoryGirl.create :scenario }
   let(:scene) { FactoryGirl.create :scene }
+  let(:npc) { FactoryGirl.create :npc}
   describe '#index' do
     it "renders a json blob containing all scenes associated with a scenario" do
-      get "/scenarios/#{scenario.id}/scenes"
+      get :index, "/scenarios/#{scenario.id}/scenes", scenario_id: scenario.id
       expect(response.body).to eq(Scene.all.to_json)
     end
     it "responds with a 200" do
       expect(response.status).to eq(200)
     end
+  end
+  describe '#update' do
+    it 'associates an existing NPC with an existing scene' do
+      put :update, "/scenes/#{scene.id}", {id: scene.id, names: [npc.name]}
+      expect(response.body).to eq(Npc.where(scene_id: scene.id))
+    end
+  end
+  xdescribe '#create' do
   end
 end
